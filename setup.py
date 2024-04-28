57de37610efa14db3bf54fd1a7b72c4b91a710fc
lshpaner <lshpaner@ucla.edu>
2024-04-27 17:59:56 -0700

    substantial refactoring of flexibility to existing functions, parsing into __init__.py, and documentation

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="kfre",
-    version="0.1.1-alpha7",
+    version="0.1.1-alpha9",
     author="Leonid Shpaner",
     author_email="Lshpaner@ucla.edu",
     description="A Python library for kidney failure risk estimation using Tangri's KFRE model",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",  # Type of the long description
-    url="https://github.com/lshpaner/kfre",  # URL to the repository or documentation
     package_dir={"": "src"},  # Directory where your package files are located
     # Automatically find packages in the specified directory
     packages=find_packages(where="src"),
+    project_urls={  # Optional
+        "Author Website": "https://www.leonshpaner.com",
+        # "Documentation": "https://example.com/project-documentation",
+        "Source Code": "https://github.com/lshpaner/kfre",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
