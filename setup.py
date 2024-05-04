c92f759f8b65283d1109b031353e362e847f77b4
lshpaner <lshpaner@ucla.edu>
2024-05-04 09:17:48 -0700

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kfre",
-    version="0.1.2_b4",
+    version="0.1.2_b5",
     author="Leonid Shpaner",
     author_email="Lshpaner@ucla.edu",
     description="A Python library for kidney failure risk estimation using Tangri's KFRE model",
