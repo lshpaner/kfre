4909208de724e2cc12084e512f2f179f77d69879
lshpaner <lshpaner@ucla.edu>
2024-06-10 17:43:00 -0700

    concatenated exceptions with \n instead of ; char, bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kfre",
-    version="0.1.7rc1",
+    version="0.1.7",
     author="Leonid Shpaner",
     author_email="lshpaner@ucla.edu",
     description="A Python library for estimating kidney failure risk using the KFRE model developed by Tangri et al.",
