af61df06657950f60d50e4bb6505338f6fbcc1a9
lshpaner <lshpaner@ucla.edu>
2024-06-10 17:14:14 -0700

    added exceptions to kfre_person(), included acknowledgements, bump version to 0.1.7b

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kfre",
-    version="0.1.6",
+    version="0.1.7b",
     author="Leonid Shpaner",
     author_email="lshpaner@ucla.edu",
     description="A Python library for estimating kidney failure risk using the KFRE model developed by Tangri et al.",
