1e22c2f12f351a77cb9026f5ce94289117093c25
lshpaner <lshpaner@ucla.edu>
2024-07-12 19:19:36 -0700

    added kfre_docs to this repo as separate branch

--- source/conf.py
+++ source/conf.py
@@ -1,12 +1,7 @@
-import sys
-
-sys.setrecursionlimit(30000)
-
-# import sphinx_rtd_theme
-from docutils import nodes
-from docutils.parsers.rst import roles
+import sphinx_rtd_theme
 
 # Configuration file for the Sphinx documentation builder.
+#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -28,46 +23,36 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
+    # "sphinxcontrib.bibtex",
 ]
 
+# Add this line to specify the bibliography file
+# bibtex_bibfiles = ["references.bib"]
+
+extensions.append("sphinx.ext.intersphinx")
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
 }
 
-todo_include_todos = True
+copybutton_prompt_text = r">|\$ "
+copybutton_prompt_is_regexp = True
+
 
 templates_path = ["_templates"]
 exclude_patterns = []
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-
-html_theme_options = {
-    "collapse_navigation": True,
-    "sticky_navigation": True,
-    "navigation_depth": 3,
-    "includehidden": True,
-    "titles_only": False,
-}
-
 html_static_path = ["_static"]
+
+# If your documentation is served from a subdirectory, set this to the subdirectory path
 html_show_sourcelink = False
 
-# Example of setting a Pygments style for syntax highlighting
-pygments_style = "default"
 
-
-def bold_italic(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    node = nodes.strong(text=text)
-    node.children = [nodes.emphasis(text=text)]
-    return [node], []
-
-
-roles.register_local_role("bolditalic", bold_italic)
-
-html_css_files = [
-    "custom.css",
-]
+def setup(app):
+    app.add_css_file("custom.css")
+    app.add_js_file("custom.js")
