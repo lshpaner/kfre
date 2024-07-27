ffd11fe5d3d012465c9b0da822383ae72cf51f93
lshpaner <lshpaner@ucla.edu>
2024-07-27 13:05:05 -0700

    added UTF8 encoding fix in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Leonid Shpaner",
     author_email="lshpaner@ucla.edu",
     description="A Python library for estimating kidney failure risk using the KFRE model developed by Tangri et al.",
-    long_description=open("min_readme.md").read(),
+    long_description=open("min_readme.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",  # Type of the long description
     package_dir={"": "src"},  # Directory where your package files are located
     # Automatically find packages in the specified directory
