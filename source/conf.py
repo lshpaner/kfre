0d0fbabad97c8cbc77241ebb0acb38dcd5ebb866
lshpaner <lshpaner@ucla.edu>
2024-12-14 08:26:24 -0800

    updated AUC calc in docs

--- source/conf.py
+++ source/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # If your documentation is served from a subdirectory, set this to the subdirectory path
 html_show_sourcelink = False
